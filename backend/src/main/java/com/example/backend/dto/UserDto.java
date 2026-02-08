b897f373feb798561c240861a6ded80f04e90b16
xennkavier13 <kennxavier15@gmail.com>
2026-02-08 19:58:28 +0800

    feat(user): implemented and tested user endpoint

--- backend/src/main/java/com/example/backend/dto/UserDto.java
+++ backend/src/main/java/com/example/backend/dto/UserDto.java
@@ -1,9 +1,21 @@
 package com.example.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+    private int userId;
     private String username;
     private String email;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
 }
